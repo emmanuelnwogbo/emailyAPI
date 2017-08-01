ccb5b63e8bdd88d369dc47772e7cd6c0cfae9d9b
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-08-01 16:05:56 +0000

    fourth commit

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback"
+			callbackURL:
+				"https://fathomless-dawn-96284.herokuapp.com/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then(existingUser => {
