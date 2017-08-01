341e75f6c21e2367ee40fabd6d496e9e04413446
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-08-01 15:53:43 +0000

    third commit

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,20 @@
 const passport = require("passport");
+const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(
 	new GoogleStrategy(
@@ -11,9 +24,16 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log("access token", accessToken);
-			console.log("refresh Token", refreshToken);
-			console.log("profile", profile);
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					//do something here
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
 	)
 );
