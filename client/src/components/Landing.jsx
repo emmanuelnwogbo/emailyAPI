6be21fe75b5048681a91d34fb5e1223df10832b3
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-08-03 12:30:28 +0000

    fourth commit with client side app

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>Emaily!</h1>
+			Collect feedback forms of your users
+		</div>
+	);
+};
+
+export default Landing;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,14 +8,21 @@ module.exports = app => {
 		})
 	);
 
-	app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get(
+		"/auth/google/callback",
+		passport.authenticate("google"),
+		(req, res) => {
+			res.redirect("/surveys");
+		}
+	);
 
 	app.get("/api/logout", (req, res) => {
 		//passport also adds a couple of other properties to the
 		//req object as well that we can use to manipulate the
 		//authentication status of the user; one of them is req.logout
 		req.logout();
-		res.send(req.user);
+		//res.send(req.user);
+		res.redirect("/");
 	});
 
 	app.get("/api/current_user", (req, res) => {
